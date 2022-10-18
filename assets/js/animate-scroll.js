1c263d4378fba43127f94b35ab78e6077dab9351
@diogenesdornelles <diogenes.dornelles@gmail.com>
2022-10-17 23:59:19 -0300

    2022-10-17

--- assets/js/animate-scroll.js
+++ assets/js/animate-scroll.js
@@ -1,5 +1,35 @@
-function animationScroll () {
+function showSkills() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+    });
+  });
 
+  const hiddenElements = document.querySelectorAll('.hidden');
+
+  hiddenElements.forEach((element) => observer.observe(element));
 }
 
-animationScroll()
+function modifyPhoto() {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+          entry.target.classList.add('modify');
+      } else {
+          entry.target.classList.remove('modify');
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll('.image');
+  hiddenElements.forEach((element) => observer.observe(element));
+}
+
+modifyPhoto()
+showSkills()
